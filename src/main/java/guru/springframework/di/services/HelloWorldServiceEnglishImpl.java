3d2939e38715f8a9384db1ec1b635d52976c01f6
taniaJude <jude.m.tania@gmail.com>
2020-01-06 09:19:31 +0200

    Changed hello world message

--- src/main/java/guru/springframework/di/services/HelloWorldServiceEnglishImpl.java
+++ src/main/java/guru/springframework/di/services/HelloWorldServiceEnglishImpl.java
@@ -3,6 +3,6 @@ package guru.springframework.di.services;
 public class HelloWorldServiceEnglishImpl implements HelloWorldService {
     @Override
     public String getGreeting() {
-        return "Hello World!";
+        return "Helloo World!";
     }
 }
